2ec5e5a83930963fce6679c07ef8852e4381d63e
Rakveerchand <rakveer@coffeebeans.io>
2024-04-17 18:30:08 +0530

    test

--- src/aws_lambda.py
+++ src/aws_lambda.py
@@ -6,12 +6,13 @@ from common.aws_clients import get_client
 
 
 def run(aws_parameters: dict[str, AwsParameter], input_lambda_name):
-    lambda_client = get_client(service_name='lambda',
-                               region_name=os.getenv('AWS_REGION'),
-                               aws_access_key=os.getenv('AWS_ACCESS_KEY'),
-                               aws_secret_key=os.getenv('AWS_SECRET_KEY'),
-                               )
+    # lambda_client = get_client(service_name='lambda',
+    #                            region_name=os.getenv('AWS_REGION'),
+    #                            aws_access_key=os.getenv('AWS_ACCESS_KEY'),
+    #                            aws_secret_key=os.getenv('AWS_SECRET_KEY'),
+    #                            )
     print(aws_parameters, input_lambda_name)
+
 
 
 if __name__ == '__main__':
